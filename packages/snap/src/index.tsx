085a592d6a6e57c1f489fa70c952771027ce47e0
snf <snf@users.noreply.github.com>
2025-02-27 01:48:04 +0000

    wip local qr

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,6 +1,9 @@
 import type { EIP1559Transaction, OnRpcRequestHandler, OnTransactionHandler, OnSignatureHandler } from '@metamask/snaps-sdk';
 import { Box, Text, Bold, Heading, Image, Link} from '@metamask/snaps-sdk/jsx';
 
+// @ts-ignore
+import * as qrcodegen from './qrcodegen.js';
+
 declare global {
   interface Window {
     ethereum: any;
@@ -35,6 +38,27 @@ async function getBitfindingQR(params: {
   return await bitfindingRequest.json();
 }
 
+// Returns a string of SVG code for an image depicting the given QR Code, with the given number
+	// of border modules. The string always uses Unix newlines (\n), regardless of the platform.
+	function toSvgString(qr: qrcodegen.QrCode, border: number, lightColor: string, darkColor: string): string {
+		if (border < 0)
+			throw new RangeError("Border must be non-negative");
+		let parts: Array<string> = [];
+		for (let y = 0; y < qr.size; y++) {
+			for (let x = 0; x < qr.size; x++) {
+				if (qr.getModule(x, y))
+					parts.push(`M${x + border},${y + border}h1v1h-1z`);
+			}
+		}
+		return `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
+	<rect width="100%" height="100%" fill="${lightColor}"/>
+	<path d="${parts.join(" ")}" fill="${darkColor}"/>
+</svg>
+`;
+	}
+
 /**
  * On Transaction
  */
@@ -45,18 +69,22 @@ export const onTransaction: OnTransactionHandler = async (data) => {
   // TODO: remove me
   console.log(`Tx: ${JSON.stringify(transaction)}`)
   
-  const bitfindingData = await getBitfindingQR({
-    chainId,
-    transaction: encodedTransaction,
-    origin: transactionOrigin ?? '',
-  });
+
+  // Name abbreviated for the sake of these examples here
+    const QRC = qrcodegen.QrCode;
+
+    // Simple operation
+    const qr0 = "Hello, world!";
+
+    const qrCode = QRC.encodeText(qr0, qrcodegen.QrCode.Ecc.MEDIUM);
+    const svg = toSvgString(qr0, 4, "white", "black");
 
   return {
     content: (
       <Box>
         <Heading>Bitfinding second factor</Heading>
-        <Text>{bitfindingData.url}</Text>
-        <Image src={bitfindingData.svg} />
+        <Text>{qr0}</Text>
+        {/* <Image src={svg} /> */}
       </Box>
     ),
     severity: 'critical',
@@ -71,10 +99,10 @@ export const onSignature: OnSignatureHandler = async (data) => {
   // TODO: remove me
   console.log(`Tx: ${JSON.stringify(signature)}`);
   
-  const bitfindingData = await getBitfindingQR({
-    signature: encodedSignature,
-    origin: signatureOrigin ?? '',
-  });
+//   const bitfindingData = await getBitfindingQR({
+//     signature: encodedSignature,
+//     origin: signatureOrigin ?? '',
+//   });
   
   return {
     content: (
