2bd9c4f1884b05461acd69bad1fb161f8536bc92
Felipe Manzano <felipe.andres.manzano@gmail.com>
2025-06-14 20:09:45 -0300

    Remove dapp->snap rpc thing

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -368,39 +368,6 @@ export const onSignature: OnSignatureHandler = async (data) => {
 };
 
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = async ({
-  origin,
-  request,
-}) => {
-  switch (request.method) {
-    case 'eth_sendTransaction':
-      return await snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'alert',
-          content: (
-            <Box>
-              <Heading>Unblind Alert</Heading>
-              <Text>Something happened in the system.</Text>
-            </Box>
-          ),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
-  }
-};
-
 type UserInfo = {
   userId: string;
   qrCode: string;
